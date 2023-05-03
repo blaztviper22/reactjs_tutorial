aaf747bc62e1d25ef331ed568ae72b7715b953aa
berthem <blaztviper22@gmail.com>
2023-05-03 12:44:45 +0800

    initial commit

---
+++ src/Items.jsx
@@ -0,0 +1,24 @@
+import SingleItem from './SingleItem';
+import { useQuery } from '@tanstack/react-query';
+import customFetch from './utils';
+
+const Items = ({ items }) => {
+  const {isLoading, data} = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => customFetch.get('/')
+  });
+
+if(isLoading) {
+  return <p style={{marginTop: '1rem'}}>...Loading</p>
+}
+
+  
+  return (
+    <div className='items'>
+      {items.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+export default Items;
